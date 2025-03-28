462145bf5866c85a8ccfc6beb49ac6e778babc2d
eliandy <rymer.eliandy@gmail.com>
2025-03-28 23:56:55 +0100

    Page de détail KPI avec commentaires et formulaire

--- pilot/urls.py
+++ pilot/urls.py
@@ -3,4 +3,6 @@ from .views import DashboardView
 
 urlpatterns = [
     path('', DashboardView.as_view(), name='dashboard'),
+    path('kpi/<int:pk>/', KPIDetailView.as_view(), name='kpi-detail'),
+
 ]
--- pilot/views.py
+++ pilot/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import TemplateView
-from .models import KPI
+from django.views.generic import TemplateView,DetailView
+from django.views.generic.edit import FormMixin
+from django.urls import reverse
+from .models import KPI, Commentaire
+from django import forms
 
 class DashboardView(TemplateView):
     template_name = 'pilot/dashboard.html'
@@ -11,3 +14,36 @@ class DashboardView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['kpis'] = KPI.objects.all().order_by('-date')
         return context
+
+# Formulaire pour les commentaires
+class CommentaireForm(forms.ModelForm):
+    class Meta:
+        model = Commentaire
+        fields = ['contenu']
+        widgets = {
+            'contenu': forms.Textarea(attrs={
+                'placeholder': "Ajoutez un commentaire...",
+                'class': 'w-full p-2 border rounded-md',
+                'rows': 3,
+            })
+        }
+
+class KPIDetailView(FormMixin, DetailView):
+    model = KPI
+    template_name = 'pilot/kpi_detail.html'
+    context_object_name = 'kpi'
+    form_class = CommentaireForm
+
+    def get_success_url(self):
+        return reverse('kpi-detail', kwargs={'pk': self.object.pk})
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form()
+        if form.is_valid():
+            commentaire = form.save(commit=False)
+            commentaire.kpi = self.object
+            commentaire.utilisateur = request.user
+            commentaire.save()
+            return self.form_valid(form)
+        return self.form_invalid(form)
