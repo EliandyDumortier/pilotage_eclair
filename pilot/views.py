45e5eae3b10c313b3195099dc9a758bc10d396dc
eliandy <rymer.eliandy@gmail.com>
2025-03-28 23:27:30 +0100

    Ajout de DashboardView et configuration des URLs

--- pilot/views.py
+++ pilot/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import TemplateView
+from .models import KPI
+
+class DashboardView(TemplateView):
+    template_name = 'dashboard.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['kpis'] = KPI.objects.all().order_by('-date')
+        return context
